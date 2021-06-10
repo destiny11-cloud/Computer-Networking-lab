9695a3b27a7d9b3d0212b70ab443f3f716187bb0
lsp233 <59755348+destiny11-cloud@users.noreply.github.com>
2021-06-11 02:13:38 +0800

    Add files via upload

---
+++ music_player/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    class User
+    {
+        private string username;
+        private string password;
+        public User(string s1,string s2)
+        {
+            this.username = s1;
+            this.password = s2;
+        }
+        public string getUsername()
+        {
+            return username;
+        }
+        public string getPassword()
+        {
+            return password;
+        }
+    }
+}
